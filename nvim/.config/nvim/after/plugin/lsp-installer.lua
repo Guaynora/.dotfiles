58949dff7d8e38bfa241fc2230e3e06e8e1d1a7f
Jonathan Guaynora <guaynora13@gmail.com>
2022-07-06 12:32:29 -0500

    update config for lsp and treesitter

---
+++ nvim/.config/nvim/after/plugin/lsp-installer.lua
@@ -0,0 +1,24 @@
+
+local ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not ok then
+  return
+end
+lsp_installer.setup({
+    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+    ui = {
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+    },
+    keymaps = {
+          toggle_server_expand = "<CR>",
+          install_server = "i",
+          update_server = "u",
+          check_server_version = "c",
+          update_all_servers = "U",
+          check_outdated_servers = "C",
+          uninstall_server = "X",
+    },
+})
